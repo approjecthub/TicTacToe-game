fbf03e307a722897114141893396fb428f921515
Anirban Pal <anirbanpal1997@gmail.com>
2021-04-17 13:35:55 +0530

    UI working good, but winner deciding logic till not added

---
+++ README.md
@@ -0,0 +1,3 @@
+# TicTacToe-game
+
+### Reference : web dev simplified(https://www.youtube.com/watch?v=Y-GkMjUZsmM&t=1704s)
---
+++ script.js
@@ -0,0 +1,48 @@
+const cellList = Array.from(document.getElementsByClassName('cell'))
+let countMarked = 0
+
+let cellvisit = []
+
+cellList.forEach((currentCell,i)=>{
+    cellvisit.push(0)
+    currentCell.addEventListener('click', ()=>{      
+        cellvisit[i] = 1
+        let sign = getSign()
+        let classList = currentCell.classList.value.split(' ')
+            removeExtraClasses(classList, currentCell)       
+            currentCell.classList.add(sign)
+        countMarked += 1
+        markWave()
+
+    })
+})
+
+function getSign(){
+    let sign = 'o'
+    if(countMarked%2==0){
+        sign = 'x'
+    }
+    return sign
+}
+
+function removeExtraClasses(classList, currentCellList){
+    classList.forEach((className)=>{
+        if(className!="cell") currentCellList.classList.remove(className)
+    })
+}
+function markWave(){
+    let sign = getSign()
+    if(countMarked%2==0){
+        sign = 'x'
+    }
+    for(let i=0; i<cellvisit.length;i++){
+        if(cellvisit[i]==0){
+            let classList = cellList[i].classList.value.split(' ')
+            removeExtraClasses(classList, cellList[i])
+            cellList[i].classList.add('wave')
+            cellList[i].classList.add(sign)
+        }      
+    }
+}
+
+markWave()
